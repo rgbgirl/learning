4bfe11e8afd4af513e973b1da5fa521888321091
rgbgirl <yoe831@sina.com>
2017-09-12 12:04:00 +0800

    a new hello python file!

---
+++ hello.py
@@ -0,0 +1,21 @@
+# coding :utf-8
+'test'
+__author__ ='ywbin'
+
+import sys
+
+def test():
+    args = sys.argv
+    if len(args) ==1:
+        print 'Hello world!'
+        pass
+    elif len(args) ==2:
+        print 'Hello, %s!' %args[1]
+        pass
+    else:
+        print 'Too many arguments!'
+        pass
+    pass
+if __name__ == '__main__':
+    test()
+    pass
